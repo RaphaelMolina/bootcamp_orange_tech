8b3fd70a0ec43a0ef25b67bc3725241796cf3e4e
RaphaelMolina <manson1307@gmail.com>
2022-11-15 13:50:27 -0300

    Alteração.

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -2,24 +2,22 @@ const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
 
 const maxRecords = 151;
-const limit = 4;
+const limit = 12;
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         pokemonList.innerHTML += pokemons.map((pokemon) => 
-            `<a href="details.html" target="_parent">
-                <li class="pokemon ${pokemon.type}">
-                    <span class="number">#${pokemon.number}</span>
-                    <span class="name">${pokemon.name}</span>
-                    <div class="detail">
-                        <ol class="types">
-                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                        </ol>
-                        <img src="${pokemon.photo}" alt="${pokemon.name}">
-                    </div>
-                </li>
-            </a>`).join('');
+            `<li class="pokemon ${pokemon.type}" onclick="openModal(${pokemon.number})">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>`).join('');
     });
 }
 
