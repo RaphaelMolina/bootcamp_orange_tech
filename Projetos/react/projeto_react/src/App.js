90da4e80608d16f623ca892a2bd3e6679fecdda5
RaphaelMolina <manson1307@gmail.com>
2022-11-20 13:49:56 -0300

    Alteração.

--- Projetos/react/projeto_react/src/App.js
+++ Projetos/react/projeto_react/src/App.js
@@ -1,11 +1,10 @@
-import Button from "./components/Button";
+import Button from './components/Button';
+
 function App() {
   return (
     <div className="App">
       <h1>Olá React</h1>
       <Button title="Entrar"/>
-      <Button title="Acessar"/>
-      <Button title="Logar"/>
     </div>
   );
 }
--- Projetos/react/projeto_react/src/components/Button/index.js
+++ Projetos/react/projeto_react/src/components/Button/index.js
@@ -1,5 +1,4 @@
-export default function Button(title) {
-    return (
-        <button>{title}</button>
-    );
- }
+export default function Button(props) {
+    const { title } = props;
+    return <button>{title}</button>;
+}
